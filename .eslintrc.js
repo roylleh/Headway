2aa9a4fa2a22f266c7edab26fdc1fb474450f724
roylleh <royllehzyxas@yahoo.com>
2023-08-15 18:33:25 -0700

    Add config stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/strict",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "prettier",
   ],
-  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
+  plugins: ["@typescript-eslint"],
   root: true,
 };
